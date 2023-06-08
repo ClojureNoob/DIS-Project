95241196a8d157678de98dc8b1713feb8458f37a
simvad <simon.vadsholt@gmail.com>
2023-06-08 15:54:01 +0200

    Updated with single price db

--- db_setup.py
+++ db_setup.py
@@ -14,15 +14,8 @@ with open('companies.csv', 'r',encoding='utf-8') as f:
     cursor.copy_expert("COPY companies FROM STDIN WITH (FORMAT CSV, HEADER 1)", f)
 conn.commit()
 
-#go through the folder stocks and make a table for each stock
-for filename in os.listdir('stocks'):
-    #asc.csv and true.csv escaped because psycopg2 sees them as keywords. We should probably find a better solution
-    # See fx. this: https://stackoverflow.com/questions/43111996/why-postgresql-does-not-like-uppercase-table-names
-    if filename.endswith(".csv") and filename != 'ASC.csv' and filename != 'TRUE.csv':
-        symbol = filename.split('.')[0].lower()
-        cursor.execute(f"DROP TABLE IF EXISTS {symbol};")
-        #Volume should be type BIGINT but some volume values are apparently floats (.0)
-        cursor.execute(f"CREATE TABLE {symbol} (date DATE PRIMARY KEY, open FLOAT, high FLOAT, low FLOAT, close FLOAT, adj_close FLOAT, volume FLOAT);")
-        with open(f'stocks/{filename}', 'r',encoding='utf-8') as f:
-            cursor.copy_expert(f"COPY {symbol} FROM STDIN WITH (FORMAT CSV, HEADER 1)", f)
-        conn.commit()
+cursor.execute("DROP TABLE IF EXISTS prices;")
+cursor.execute("CREATE TABLE prices (symbol VARCHAR(10), date DATE, price FLOAT);")
+
+with open('prices.csv', 'r',encoding='utf-8') as f:
+    cursor.copy_expert("COPY prices FROM STDIN WITH (FORMAT CSV, HEADER 1)", f)
